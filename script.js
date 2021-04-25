cbc39b3695dc3f676559942716d28d9f028866f5
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-04-26 01:44:34 +0200

    add structure

---
+++ script.js
@@ -0,0 +1,68 @@
+var zucchine = [
+    {
+        varieta:"golden",
+        peso: 4,
+        lunghezza: 11,
+    },
+
+    {
+        varieta:"fiorentine",
+        peso: 3,
+        lunghezza: 10,
+    },
+
+    {
+        varieta:"romanesche",
+        peso: 2,
+        lunghezza: 8,
+    },
+
+    {
+        varieta:"golden",
+        peso: 5,
+        lunghezza: 13,
+    },
+    
+    {
+        varieta:"tonde",
+        peso: 6,
+        lunghezza: 8,
+    },
+
+    {
+        varieta:"romanesche",
+        peso: 4,
+        lunghezza: 10,
+    },
+
+    {
+        varieta:"fiorentine",
+        peso: 5,
+        lunghezza: 12,
+    },
+
+    {
+        varieta:"nere",
+        peso: 3,
+        lunghezza: 9,
+    },
+
+    {
+        varieta:"tonde",
+        peso: 5,
+        lunghezza: 8,
+    },
+    
+    {
+        varieta:"nere",
+        peso: 2,
+        lunghezza: 8,
+    },
+]
+
+for (var peso in zucchine) {
+    console.log(key + ":" + zucchine[peso])
+}
+
+
+var sommaPeso 
